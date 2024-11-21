5e196503aee3dd675a1f00bbcad573a99b0204f2
dar_san <d7780984@gmail.com>
2024-11-21 11:17:29 +0530

    Learned bool, mutablity

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
 fn main() {
-    let a :i8 = 7;
-    print!("a:{}\n",a);
+    let a:i8 = 7;
+    let b: i16 = 23;
+    print!("a:{}  b:{}\n",b,a);
+    let mut c:bool = false;
+    if c {
+        print!("C is a True Bool.");
+
+    }
+    else {
+        print!("C is not a False(false)Bool\n");
+        c = true;
+        if c {
+        print!("Now C is a true Bool.\n");
+        }
+
+    }
+
+    /*for c:i32 in 0..1000{
+        a = a + 100;
+    };*/
 }

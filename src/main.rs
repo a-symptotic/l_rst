6e7d6e347744eabeab029479f16dc1fa249c406e
dar_san <d7780984@gmail.com>
2024-11-25 21:44:27 +0530

    Okay Learned String::from() used to create string

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 fn main() {
     let a:i8 = 7;
     let b: i16 = 23;
+    let my_str:String =String::from("Hello, World!");
+    
     print!("a:{}  b:{}\n",b,a);
     let mut c:bool = false;
     if c {
         print!("C is a True Bool.");
-
     }
     else {
         print!("C is not a False(false)Bool\n");
@@ -13,14 +14,13 @@ fn main() {
         if c {
         print!("Now C is a true Bool.\n");
         }
-        
-
    }
-   for _ in -123..-100{
-    println!("WTF");
-   }
-   let my_str:String = String::from("This Is all i have been waitintg for.");
-
-    print!("{}\n",my_str);
+   for _ in -123..-110{
+    println!("Loop Dekha");}
+   
+   let my_str2:String = String::from("This Is all i have been waitintg for.");
+   let jhut:String = String::from("This seems UnMemoriable");
+   println!("Seems Bad {}",jhut);
+   print!("{}\n{}\n",my_str2,my_str);
 
 }
